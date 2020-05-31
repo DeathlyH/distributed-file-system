89f909c4cc99f0aea803b91fe9dcae989543c8a7
Zhihua Cai <zhihuacai343@gmail.com>
2020-05-31 13:43:46 -0700

    add msgpack for the logRecord

--- common/common.cc
+++ common/common.cc
@@ -5,17 +5,17 @@ long GetCurrentTimestamp() {
   return static_cast<long>(t);
 }
 
-LogRecord::LogRecord(long timestamp, int log_id, const std::string& operation_name,
-                     const std::string& file_name, const std::string& operation_content,
-                     const std::string& log_owner) {
-  this->timestamp = timestamp;
-  this->log_id = log_id;
-  this->operation_name = operation_name;
-  this->file_name = file_name;
-  this->operation_content = operation_content;
-  this->log_owner = log_owner;
-}
+//LogRecord::LogRecord(long timestamp, int log_id, const std::string& operation_name,
+//                     const std::string& file_name, const std::string& operation_content,
+//                     const std::string& log_owner) {
+//  this->timestamp = timestamp;
+//  this->log_id = log_id;
+//  this->operation_name = operation_name;
+//  this->file_name = file_name;
+//  this->operation_content = operation_content;
+//  this->log_owner = log_owner;
+//}
 
-PayLoad::PayLoad(const std::vector<LogRecord>& log_record_vector, int commit_point) :
-                                                                log_record_vector(log_record_vector),
-                                                                commit_point(commit_point) {}
+//PayLoad::PayLoad(const std::vector<LogRecord>& log_record_vector, int commit_point) :
+//                                                                log_record_vector(log_record_vector),
+//                                                                commit_point(commit_point) {}
--- common/common.h
+++ common/common.h
@@ -1,27 +1,41 @@
-#ifndef common
-#define common
+#ifndef __COMMON__
+#define __COMMON__
 
 #include <ctime>
 #include <string>
 #include <vector>
+#include "msgpack.hpp"
+
+
+#define HARP_COMMIT (1)
+#define HARP_ABORT (2)
+#define HARP_NONE (0)
 
 long GetCurrentTimestamp();
 
-struct LogRecord {
-  LogRecord(long timestamp, int log_id, const std::string& operation_name,
-            const std::string& file_name, const std::string& operation_content,
-            const std::string& log_owner);
-  long timestamp;
+enum EventStatus{
+	NONE,
+	COMMIT,
+	ABORT
+};
+
+typedef struct  {
+//  LogRecord(long timestamp, int log_id, const std::string& operation_name,
+//            const std::string& file_name, const std::string& operation_content,
+//            const std::string& log_owner);
+//  long timestamp;
   int log_id;
   std::string operation_name;
   std::string file_name;
-  std::string operation_content;
-  std::string log_owner;
-};
+  std::string file_content;
+//  std::string log_owner;
+  int status;
+  MSGPACK_DEFINE(log_id,operation_name, file_name, file_content, status);
+} LogRecord;
 
 
 struct PayLoad {
-  PayLoad(const std::vector<LogRecord>& log_record_vector, int commit_point);
+//  PayLoad(const std::vector<LogRecord>& log_record_vector, int commit_point);
   std::vector<LogRecord> log_record_vector;
   int commit_point;
 };
